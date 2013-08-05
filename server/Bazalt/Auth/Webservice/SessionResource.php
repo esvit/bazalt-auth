af0c942a45cc6e1a4c39cc90c530467b3776ba1e
Vitalii Savchuk <esvit666@gmail.com>
2013-08-05 10:54:53 +0300

    fix session resource

--- server/Bazalt/Auth/Webservice/SessionResource.php
+++ server/Bazalt/Auth/Webservice/SessionResource.php
@@ -3,36 +3,15 @@
 namespace Bazalt\Auth\Webservice;
 use Bazalt\Auth\Model\User;
 use Bazalt\Data\Validator;
-use Tonic\Response;
+use Bazalt\Rest\Response;
 
 /**
  * SessionResource
  *
  * @uri /auth/sessions
  */
-class SessionResource extends \Tonic\Resource
+class SessionResource extends \Bazalt\Rest\Resource
 {
-    /**
-     * Condition method to turn output into JSON
-     */
-    protected function json()
-    {
-        $this->before(function ($request) {
-            if ($request->contentType == "application/json") {
-                $request->data = json_decode($request->data);
-            }
-        });
-        $this->after(function ($response) {
-            $response->contentType = "application/json";
-
-            if (isset($_GET['jsonp'])) {
-                $response->body = $_GET['jsonp'].'('.json_encode($response->body).');';
-            } else {
-                $response->body = json_encode($response->body);
-            }
-        });
-    }
-
     /**
      * @method GET
      * @json
@@ -50,10 +29,10 @@ class SessionResource extends \Tonic\Resource
     public function login()
     {
         $user = null;
-        $data = new Validator((array)$this->request->data);
+        $data = Validator::create($this->request->data);
         $data->field('password')->required();
         $data->field('email')->required()->validator('exist_user', function($value) use (&$user, $data) {
-            $user = User::getUserByLoginPassword($value, $data->getData('password'), true);
+            $user = User::getUserByLoginPassword($value, $data['password'], true);
             return ($user != null);
         }, 'User with this email does not exists');
 
